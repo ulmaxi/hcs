d451a13d3cd565eee272ad70693a9ff6b7aab08e
Oyegoke Abiodun <abiodunogundijo@gmail.com>
2020-03-13 12:37:28 +0100

    configured the users service for users management

--- apps/users/src/app.module.ts
+++ apps/users/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersAdmininistrationModule, SuperUsersAdmininistrationModule } from '@ulmax/users-admininistration';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configDatabase } from '@ulmax/server-shared';
 
 @Module({
   imports: [
     UsersAdmininistrationModule, 
-    SuperUsersAdmininistrationModule]
+    SuperUsersAdmininistrationModule,
+    TypeOrmModule.forRoot(configDatabase(process.env.NODE_ENV))
+  ]
 })
 export class AppModule {}
